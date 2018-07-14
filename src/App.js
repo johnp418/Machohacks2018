f8a1eb4845347990729e8e4f0e641ec9b01057a0
JohnPark <jongmanpark418@gmail.com>
2018-07-14 14:45:36 -0700

    - Contract WIP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+import Web3 from "web3";
+import WorkChain from "./ethereum/build/WorkChain.json";
+
+const CONTRACT_ADDRESS = "0x09d118BC732f1d1E43B8292B021bDA1271F1870e";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      
-      </div>
+  async componentDidMount() {
+    const web3 = new Web3(window.web3.currentProvider);
+    console.log("Web3 ", window.web3.currentProvider);
+
+    const workChain = new web3.eth.Contract(
+      JSON.parse(WorkChain.interface),
+      // Deployed contract address
+      CONTRACT_ADDRESS
     );
+    console.log("WorkChain ", workChain);
+
+    const companies = await workChain.methods.companyProfiles.call();
+    console.log("companies ", companies);
+  }
+
+  render() {
+    return <div className="App" />;
   }
 }
 
---
+++ src/ethereum/contracts/WorkChain.sol
@@ -0,0 +1,74 @@
+pragma solidity ^0.4.17;
+
+contract WorkChain {
+
+    struct CompanyProfile {
+        address id;
+        string name;
+    }
+    
+    struct WorkExperience {
+        address employer;
+        string title;
+        string description;
+        string startDate;
+        string endDate;
+    }
+
+    struct PersonalProfile {
+        string name;
+    }
+    
+    address[] public companies;
+    mapping(address => CompanyProfile) companyProfileMap;
+    
+    mapping(address => PersonalProfile) personalProfileMap;
+    
+    mapping(address => WorkExperience[]) workExperienceMap;
+
+    constructor() public {
+
+    }
+
+    function getCompanies() public view returns (address[]) {
+        return companies;
+    }
+    
+    function getCompanyProfile(address id) public view returns (address, string) {
+        return (companyProfileMap[id].id, companyProfileMap[id].name);
+    }
+    
+    // function getPersonalProfile(address id)  public view returns (address, string) {
+    //     return (companyProfileMap[id].id, companyProfileMap[id].name);
+    // }
+
+    function createCompanyProfile(address id, string name) public {
+        CompanyProfile memory newCompanyProfile = CompanyProfile({
+            id: id,
+            name: name
+        });
+        companies.push(id);
+        companyProfileMap[id] = newCompanyProfile;
+    }
+    
+    function createPersonalProfile(address id, string name) public {
+        PersonalProfile memory newPersonalProfile = PersonalProfile({
+            name: name
+        });
+        personalProfileMap[id] = newPersonalProfile;
+    }
+    
+    function addWorkExperience(address personId, address companyId, string title, string description,
+        string startDate, string endDate) public {
+            WorkExperience memory experience = WorkExperience({
+                employer: companyId,
+                title: title,
+                description: description,
+                startDate: startDate,
+                endDate: endDate
+            });
+            
+            // TODO: Validate
+            workExperienceMap[personId].push(experience);
+        }
+}
